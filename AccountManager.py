dab71847236bfe093757ca0246c9b406c6e88d67
faroukbmiled <fcfaroukfc@gmail.com>
2023-05-26 09:29:32 +0100

    typos

--- AccountManager.py
+++ AccountManager.py
@@ -77,8 +77,8 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     command_list = [
         ("/start", "", "Start the bot and get a welcome message."),
         ("/help or /h", "", "Display all available commands and their usage."),
-        ("/find [query]", "-> Specify a search query", "Search for credentials based on a query and display them in Username Password format."),
-        ("/findraw [query]", "-> Specify a search query", "Search for credentials based on a query and display them in raw format."),
+        ("/find or /f [query]", "-> Specify a search query", "Search for credentials based on a query and display them in Username Password format."),
+        ("/findraw or /fr [query]", "-> Specify a search query", "Search for credentials based on a query and display them in raw format."),
         ("/update or /up", "", "Combine or update all text files in the current directory."),
         ("/downloadall or /dla", "", "Download all credentials as a file."),
         ("/download or /dl [query]", "-> Specify a search query", "Download all found credentials for a query as a file."),
