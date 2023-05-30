fcdc9ef5f8f678c465fa89e3f88ace70f53eef20
faroukbmiled <fcfaroukfc@gmail.com>
2023-05-30 01:33:18 +0100

    few fixes +unknown command handler

--- AccountManagerM.py
+++ AccountManagerM.py
@@ -3,17 +3,12 @@
 import logging
 import os
 from functools import wraps
-from telegram.ext import CallbackContext
-from threading import Thread
 import codecs
 import random
 import subprocess
 from telegram import __version__ as TG_VER
-from io import BytesIO
 import secrets
 import string
-from telegram import ReplyKeyboardRemove
-
 
 
 try:
@@ -27,8 +22,8 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"{TG_VER} version of this Bot, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
-from telegram import ForceReply, Update, InputFile, BotCommand, Bot
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import ReplyKeyboardRemove, Update, InputFile, BotCommand, Bot
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, CallbackContext
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -53,6 +48,10 @@ bot_commands = [
     BotCommand("execute", "ex: /exec mv filename, /exec ls"),
     BotCommand("cmd", "Update the commands button (UI).")
 ]
+
+# unknown command handler
+async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Sorry, I don't understand, use /help to check all commands", reply_to_message_id=update.message.message_id)
 
 # admin checker => used as a decorator
 def admin_only(func):
@@ -87,17 +86,18 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     command_list = [
         ("/start", "", "Start the bot and get a welcome message."),
         ("/help or /h", "", "Display all available commands and their usage."),
-        ("/find or /f [query]", "-> Specify a search query", "Search for credentials based on a query and display them in Username Password format."),
-        ("/findraw or /fr [query]", "-> Specify a search query", "Search for credentials based on a query and display them in raw format."),
-        ("/download or /dl [query]", "-> Specify a search query", "Download all found credentials for a query as a file."),
-        ("/downloadfile or /dlf [file_name]", "-> Specify a file name", "Download a specific file from the server."),
+        ("/find or /f [query]", "➡ Specify a search query", "Search for credentials based on a query and display them in Username Password format."),
+        ("/findraw or /fr [query]", "➡ Specify a search query", "Search for credentials based on a query and display them in raw format."),
+        ("/download or /dl [query]", "➡ Specify a search query", "Download all found credentials for a query as a file."),
+        ("/downloadfile or /dlf [file_name]", "➡ Specify a file name", "Download a specific file from the server."),
         ("/ls", "", "List all files in the current directory."),
-        ("/rm or /remove [file_name]", "-> Specify a file name", "delete a specific file names [query]."),
-        ("/rn or /rename [file_name new_name] or [all|all name]", "-> Specify a file name", "rename a specific file names [query], or all files."),
-        ("/execute or /exec [command]", "-> Specify system command", "Executes system [command]."),
-        ("/password or /pass [length|default(10)] (-s) for no special charachters", "-> Specify pass length", "generates a random password."),
+        ("/rm or /remove [file_name]", "➡ Specify a file name", "Delete a specific file."),
+        ("/rn or /rename [file_name new_name] or [all|all name]", "➡ Specify a file name", "Rename a specific file or all files."),
+        ("/execute or /exec [command]", "➡ Specify system command", "Execute a system command."),
+        ("/password or /pass [length|default(10)] (-s) for no special characters", "➡ Specify pass length", "Generate a random password."),
         ("/cmd", "", "Update the commands button (UI)."),
     ]
+
 
     help_text = "Available commands:\n\n"
     for command, arguments, usage in command_list:
@@ -368,11 +368,19 @@ async def rename_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 @argument_required
 @admin_only
 async def execute_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Execute a system command."""
-    command = ' '.join(update.message.text.split()[1:])
+    """Process the execute command to execute a system command."""
     try:
-        output = subprocess.check_output(command, shell=True)
-        await update.message.reply_text(output.decode(), reply_to_message_id=update.message.message_id)
+        command = ' '.join(update.message.text.split()[1:])
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+
+        output = output.decode(ENCODING)
+        error = error.decode(ENCODING)
+
+        if output:
+            await update.message.reply_text(f"{command}: \n{output}", reply_to_message_id=update.message.message_id)
+        if error:
+            await update.message.reply_text(f"Error! \n{error}", reply_to_message_id=update.message.message_id)
     except Exception as e:
         await update.message.reply_text(f"Error: {e}", reply_to_message_id=update.message.message_id)
 
@@ -387,11 +395,11 @@ async def cmdbutton_command(update: Update, context: ContextTypes.DEFAULT_TYPE) 
             await bot.set_my_commands(bot_commands)
             await update.message.reply_text("Coammnds button has been updated!",reply_to_message_id=update.message.message_id)
     except Exception as e:
-        await update.message.reply_text(f"Error: {e}",reply_to_message_id=update.message.message_id)
+        await update.message.reply_text(f"Error {e}",reply_to_message_id=update.message.message_id)
 
 async def generate_password_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Generate a random password."""
-    length = 10
+    length = 15
     include_special_chars = True
 
     try:
@@ -403,14 +411,16 @@ async def generate_password_command(update: Update, context: ContextTypes.DEFAUL
         elif "-s" in second_arg:
             include_special_chars = False
             length = int(first_arg)
-
-    except IndexError:
-        first_arg, *args = update.message.text.split()[1:]
-        second_arg = None
-        if "-s" in first_arg:
-            include_special_chars = False
-        else:
-            length = int(first_arg)
+    except:
+        try:
+            first_arg, *args = update.message.text.split()[1:]
+            second_arg = None
+            if "-s" in first_arg:
+                include_special_chars = False
+            else:
+                length = int(first_arg)
+        except:
+            pass
 
     if include_special_chars:
         characters = string.ascii_letters + string.digits + string.punctuation
@@ -434,6 +444,7 @@ def main() -> None:
     application.add_handler(CommandHandler(["execute", "exec"], execute_command))
     application.add_handler(CommandHandler(["password", "pass"], generate_password_command))
     application.add_handler(CommandHandler("ls", ls_command))
+    application.add_handler(MessageHandler(None, unknown_command))
     application.add_handler(CommandHandler("cmd", cmdbutton_command))
     application.run_polling()
 
